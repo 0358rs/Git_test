0b9f4773d9dc92dae97b5a20cbf54ba4dacd16bf
Satoshi.Ryoke <satoshi.ryoke@gmail.com>
2018-10-18 08:06:35 +0900

    titleのmodel化、日本時刻

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,4 @@ class Blog(models.Model):
     #id = 1, 2, 3, 4
     content = models.CharField(max_length=140)
     posted_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=32)
