27a28fbb5963be66ce6e1054a41a3f5d724d2d4e
Satoshi Ryoke <satoshi.ryoke@gmail.com>
2018-11-17 18:26:11 +0900

    bug fix

--- config/settings.py
+++ config/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'd(wk=qm2@5*vt%4fc=9ok2#3j)*_o3zr%%#!3t6(-pd=c-#smn'
 DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+#import dj_database_url
+#db_from_env = dj_database_url.config()
+#DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
